49ee1225607f90965c03968c69a61d04faa56c7f
jayati89 <varshneyjayati7@gmail.com>
2025-03-06 14:13:40 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,8 @@
-function skip() {
-  console.log('skip');
+function skillsMember() {
+  return {
+    skills: ['JavaScript', 'React', 'Node', 'Express', 'MongoDB'],
+    getSkills: function() {
+      return this.skills;
+    }
+  };
 }
